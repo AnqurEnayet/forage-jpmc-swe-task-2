fd5b2e2a47889de8e4e265b8514e0fc5823bfd8e
Md Enayetullah Anqur <enayetullah@iut-dhaka.edu>
2023-07-13 00:45:04 +0600

    imported {setInterval} from timers which made the interval function run well andit fixed the graph visual as well. I also changed a bit in the ComponentDidMOunt() function which didn't bring much changes to the problem that I was having.

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -41,18 +41,18 @@ class Graph extends Component<IProps, {}> {
       timestamp: 'date',
     };
 
-    if (window.perspective) {
+    if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       // Add more Perspective configurations here.
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price]');
-      elem.setAttribute('aggregates',`{"stock":"distinct count","top_ask_price":"avg","top_bid_price":"avg","timestamp":"distinct count"}`);
+      elem.setAttribute("view", "y_line");
+      elem.setAttribute("column-pivots", '["stock"]');
+      elem.setAttribute("row_pivots", '["timestamp"]');
+      elem.setAttribute("columns", '["top_ask_price"]');
+      elem.setAttribute("aggregates",'{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}');
 
     }
   }
